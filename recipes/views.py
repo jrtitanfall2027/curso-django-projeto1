4aa6b980e952da5d11d19b803fcce3e958012ef1
reginaldojunior <rjrreginaldojunior2008@hotmail.com>
2026-03-15 14:31:09 -0300

    Commit002

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,13 @@
-#from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Reginaldo Jr',
+        'name': 'Luiz Otávio',
     })
 
 
-#def contato(request):
-#    return HttpResponse('contato')
-
-
-#def sobre(request):
-#    return HttpResponse('sobre')
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
