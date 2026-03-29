cf16581c29e1f2366d12aeb42057a165fa60fca1
reginaldojunior <rjrreginaldojunior2008@hotmail.com>
2026-03-29 14:41:33 -0300

    Commit023

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
@@ -37,4 +39,18 @@ def recipe(request, id):
 
 
 def search(request):
-    ...
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
+    })
