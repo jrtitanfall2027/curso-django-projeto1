626ab487d07f0611613ca4721ebee0d14672854f
reginaldojunior <rjrreginaldojunior2008@hotmail.com>
2026-04-07 21:56:31 -0300

    Commit038

--- authors/views.py
+++ authors/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
+from django.urls import reverse
 
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm
 
 
 def register_view(request):
@@ -10,6 +11,7 @@ def register_view(request):
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+        'form_action': reverse('authors:register_create'),
     })
 
 
@@ -30,3 +32,15 @@ def register_create(request):
         del(request.session['register_form_data'])
 
     return redirect('authors:register')
+
+
+def login_view(request):
+    form = LoginForm()
+    return render(request, 'authors/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
+
+
+def login_create(request):
+    return render(request, 'authors/pages/login.html')
