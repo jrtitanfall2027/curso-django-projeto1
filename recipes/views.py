80e3665b1182358373967c634ebfb1aea6d0e566
reginaldojunior <rjrreginaldojunior2008@hotmail.com>
2026-05-10 13:14:26 -0300

    Commit054

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
 import os
 
-from django.db.models import F, Q
+from django.db.models import Q
+from django.db.models.aggregates import Count
+from django.db.models.functions import Concat
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.http.response import Http404
@@ -14,10 +16,12 @@ PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def theory(request, *args, **kwargs):
-    recipes = Recipe.objects.values('id', 'title')
+    recipes = Recipe.objects.get_published()
+    number_of_recipes = recipes.aggregate(number=Count('id'))
 
     context = {
-        'recipes': recipes
+        'recipes': recipes,
+        'number_of_recipes': number_of_recipes['number']
     }
 
     return render(
