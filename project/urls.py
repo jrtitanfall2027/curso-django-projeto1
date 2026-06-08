d89b90a5bef7884f28d8760210f7a14bb0d6a782
reginaldojunior <rjrreginaldojunior2008@hotmail.com>
2026-06-08 20:42:26 -0300

    Commit057

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
     path('authors/', include('authors.urls')),
-]
+    ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
